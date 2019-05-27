617ca06f3a9a67774f5dad36db2c3636477d5239
Flayson <flayson.potenciano@gmail.com>
2019-05-27 17:49:34 -0300

    Projeto inicial

---
+++ src/main/java/com/flay/cursomc4/Cursomc4Application.java
@@ -0,0 +1,13 @@
+package com.flay.cursomc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc4Application.class, args);
+	}
+
+}
